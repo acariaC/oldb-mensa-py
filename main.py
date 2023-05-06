e75d19324984db8abc19e86f754e886e03b31015
acariaC <oliverkuhn13qr@gmail.com>
2023-05-06 17:59:00 +0200

    * Added methods

--- main.py
+++ main.py
@@ -3,20 +3,30 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-url = 'https://oldenburg.my-mensa.de/essen.php?v=5611274&hyp=1&lang=de&mensa=uh#uh_tag_1'
 
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Chrome(options=options)
+def scrape(url):
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
 
-driver.get(url)
+    driver.get(url)
 
-elements = driver.find_elements(By.CSS_SELECTOR, '.ui-li-heading.text2share, .text2share:not(.next)')
+    elements = driver.find_elements(By.CSS_SELECTOR, '.ui-li-heading.text2share, .text2share:not(.next)')
 
-for element in elements:
-    element = element.text.strip().replace('­', '')
-    element = re.sub(r'\(.*\)', '', element)
-    if element:
-        print(element)
+    menu_items = []
+    for element in elements:
+        item_text = element.text.strip().replace('­', '')
+        item_text = re.sub(r'\(.*\)', '', item_text)
+        if item_text:
+            menu_items.append(item_text)
 
-driver.quit()
+    driver.quit()
+
+    return menu_items
+
+
+if __name__ == '__main__':
+    url = 'https://oldenburg.my-mensa.de/essen.php?v=5611274&hyp=1&lang=de&mensa=uh#uh_tag_1'
+    menu = scrape(url)
+    for item in menu:
+        print(item)
